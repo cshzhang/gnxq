132f6649322acbd6be31e9481942f993ad525b8e
HZhang <jyjs_zh@163.com>
2016-03-29 20:29:24 +0800

    格式化log文件名

--- uart/uart.c
+++ uart/uart.c
@@ -963,9 +963,9 @@ void writeFrame2log(struct uart_frame *frame)
 	for(i = 0; i < 6; i++){
 		len += snprintf(filename + len, 64 - len, "%02x", g_mac[i]);
 	}
-	len += snprintf(filename + len, 64 - len, "%d", year);
-	len += snprintf(filename + len, 64 - len, "%d", mon);
-	len += snprintf(filename + len, 64 - len, "%d", day);
+	len += snprintf(filename + len, 64 - len, "%4d", year);
+	len += snprintf(filename + len, 64 - len, "%2d", mon);
+	len += snprintf(filename + len, 64 - len, "%2d", day);
 	len += snprintf(filename + len, 64 - len, "%s", ".log");
 	filename[len++] = '\0';
 	
